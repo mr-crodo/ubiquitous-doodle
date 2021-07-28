3439d325f40d4491346981ee8ba590c3dec2a64a
Nasib Mammadov <bduti09@gmail.com>
2021-07-28 21:45:26 +0400

    main page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,41 @@
 <template>
   <div class="home">
-      <BlogPost />
-      <BlogPost />
+      <BlogPost :post="welcomeScreen" />
+      <BlogPost 
+      :post="post"
+      v-for="(post, index) in sampleBlogPost"
+      :key="index"
+       />
+       <div class="blog-card-wrap">
+         <div class="container">
+           <h3>View More Recent Blog</h3>
+           <div class="blog-cards">
+             <BlogCards 
+             :post="post"
+             v-for="(post, index) in sampleBlogCards"
+             :key="index"
+              />
+           </div>
+         </div>
+       </div>
+       <div class="updates">
+         <div class="container">
+           <h2>never miss a post. Register for your free account today</h2>
+           <router-link class="router-button" to="#">
+             Register for Pied Doodle <Arrow class="arrow arrow-light" />
+           </router-link>
+         </div>
+       </div>
   </div>
 </template>
 
 <script>
 import BlogPost from "../components/BlogPost";
+import BlogCards from "../components/BlogCard";
+import Arrow from "../assets/Icons/arrow-right-light.svg"
 export default {
   name: "Home",
-  components: { BlogPost },
+  components: { BlogPost, BlogCards, Arrow },
   data() {
     return {
       welcomeScreen: {
@@ -21,17 +47,47 @@ export default {
       },
       sampleBlogPost: [
         {
-          title: "This is a Filler Title",
+          title: "This is a Filler Title If",
           blogHTML: "This is a Filler blog post title",
-          blogCoverPhoto: "beatiful-stories",
+          blogCoverPhoto: "beautiful-stories",
         },
         {
-          title: "This is a Filler Title",
+          title: "This is a Filler Title Else",
           blogHTML: "This is a Filler blog post title",
           blogCoverPhoto: "designed-for-everyone",
         },
       ],
+      sampleBlogCards: [
+        { blogTitle: "Blog Card #1", blogCoverPhoto: "stock-1", blogDate: "May 1, 2021"},
+        { blogTitle: "Blog Card #2", blogCoverPhoto: "stock-2", blogDate: "May 1, 2021"},
+        { blogTitle: "Blog Card #3", blogCoverPhoto: "stock-3", blogDate: "May 1, 2021"},
+        { blogTitle: "Blog Card #4", blogCoverPhoto: "stock-4", blogDate: "May 1, 2021"},
+      ]
     };
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.blog-card-wrap {
+  h3 {
+    font-weight: 300;
+    font-size: 28px;
+    margin-bottom: 32px;
+  }
+}
+
+.updates {
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 100px 25px;
+
+    @media (min-width: 800px) {
+      padding: 125px 25px;
+      flex-direction: row;
+    }
+  }
+}
+</style>
